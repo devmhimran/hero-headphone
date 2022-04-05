7671f4ec522ed885ff25b6c63918cd751060c7a1
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-04-05 14:15:16 +0600

    Added Navbar & Router

--- hero-headphone/src/App.js
+++ hero-headphone/src/App.js
@@ -1,12 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import NavigationBar from './NavigationBar/NavigationBar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home/Home';
+import Reviews from './Reviews/Reviews';
+import Dashboard from './Dashboard/Dashboard';
+import Blogs from './Blogs/Blogs';
+import About from './About/About';
+import ErrorPage from './404/ErrorPage';
 
 function App() {
   return (
     <div className="App">
      <NavigationBar></NavigationBar>
+     <hr />
+     <Routes>
+       <Route path='/' element={<Home></Home>}></Route>
+       <Route path='/reviews' element={<Reviews></Reviews>}></Route>
+       <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
+       <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+       <Route path='/about' element={<About></About>}></Route>
+       <Route path='*' element={<ErrorPage></ErrorPage>}></Route>
+     </Routes>
     </div>
+   
   );
 }
 
--- hero-headphone/src/NavigationBar/NavigationBar.js
+++ hero-headphone/src/NavigationBar/NavigationBar.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import HeaderLogo from '../assets/img/hero-headphone-logo.png';
+import './NavigationBar.css';
 
 const NavigationBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="lg">
         <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand className='navbar__logo' href="#home"><img src={HeaderLogo} alt="" /></Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
+            <Nav className="ms-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/reviews">Reviews</Nav.Link>
+                <Nav.Link href="/dashboard">Dashboard</Nav.Link>
+                <Nav.Link href="/blogs">Blogs</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Container>
+        
         </Navbar>
+        
     );
 };
 
