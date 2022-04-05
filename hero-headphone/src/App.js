bd15cc04007dd3e1bfa4dfc1b5f234c82a4664a7
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-04-05 13:49:56 +0600

    Added React Js Files

---
+++ hero-headphone/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import NavigationBar from './NavigationBar/NavigationBar';
+
+function App() {
+  return (
+    <div className="App">
+     <NavigationBar></NavigationBar>
+    </div>
+  );
+}
+
+export default App;
